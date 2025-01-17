39c289294e6ccfd706f855bc02844a046eba48e0
Obed Nyakundi <real.obedpaul@gmail.com>
2025-01-17 16:21:31 +0300

    some branding

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->favicon(asset('images/kasongo.png'))
             ->registration()
             ->login()
             ->colors([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,2 @@
+<img src="{{ asset('images/kasongo_b.png') }}" style="width:2rem; height:auto">
+<h3 class="fi-header-heading text-2xl font-bold tracking-tight text-gray-950 dark:text-white sm:text-3x" style="font-family: cursive;">Kasongo Yeye</h3>
