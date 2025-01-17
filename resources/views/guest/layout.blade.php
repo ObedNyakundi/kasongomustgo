02af819d8dae67605a0daf826d09f08f78990ba1
Obed Nyakundi <real.obedpaul@gmail.com>
2025-01-17 18:25:50 +0300

    handled the heading issue

--- resources/views/guest/layout.blade.php
+++ resources/views/guest/layout.blade.php
@@ -252,49 +252,7 @@
                   <!-- End of Footer Area -->
 
                   <!-- Navigation Area -->
-                  <div class="frame2-container43">
-                    <div class="frame2-container44">
-                      <div class="frame2-container45">
-                        <img
-                        src="{{ asset('landing/public/external/frame28-3v5n.svg') }}"
-                          alt="Frame28"
-                          class="frame2-frame7"
-                        />
-                        <div class="frame2-heading28">
-                          <span class="frame2-text51">Ruto Must Go</span>
-                        </div>
-                      </div>
-                      <div class="frame2-container46">
-                        <div class="frame2-container47">
-                          <span class="frame2-text52">Home</span>
-                        </div>
-                        <div class="frame2-container48">
-                          <span class="frame2-text53">Politics</span>
-                        </div>
-                        <div class="frame2-container49">
-                          <span class="frame2-text54">Corruption</span>
-                        </div>
-                        <div class="frame2-container50">
-                          <span class="frame2-text55">Murders</span>
-                        </div>
-                        <div class="frame2-container51">
-                          <span class="frame2-text56">Abductions</span>
-                        </div>
-                      </div>
-                      <div class="frame2-container52">
-                        <img
-                        src="{{ asset('landing/public/external/frame226-o5l.svg') }}"
-                          alt="Frame226"
-                          class="frame2-frame8"
-                        />
-                        <img
-                        src="{{ asset('landing/public/external/frame229-u1k4.svg') }}"
-                          alt="Frame229"
-                          class="frame2-frame9"
-                        />
-                      </div>
-                    </div>
-                  </div>
+                  @include('guest.navigation')
                   <!-- End of Navigation Area -->
 
                 </div>
--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,2 +1,4 @@
+<a href="{{ route('home') }}">
 <img src="{{ asset('images/kasongo_b.png') }}" style="width:2rem; height:auto">
+</a>
 <h3 class="fi-header-heading text-2xl font-bold tracking-tight text-gray-950 dark:text-white sm:text-3x" style="font-family: cursive;">Kasongo Yeye</h3>
