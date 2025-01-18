43673c74da7a69a2e3c6ab854db6b55660194a60
Obed Nyakundi <real.obedpaul@gmail.com>
2025-01-18 14:04:21 +0300

    started to work on the admin panel

--- resources/views/guest/header.blade.php
+++ resources/views/guest/header.blade.php
@@ -5,19 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Ruto Must Go</title>
         <link rel="icon" href="{{ asset('landing/public/external/icc.jpg') }}" />
-        <link rel="stylesheet" href="{{ asset('landing/styles/index.css') }}" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        
         <link
             rel="shortcut icon"
             href="{{ asset('images/kasongo.png') }}"
             type="image/x-icon"
         />
-        <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
-        />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
-            rel="stylesheet"
-        />
+
+        <!-- Styles -->
+        @include('guest.styles')
     </head>
--- resources/views/guest/styles.blade.php
+++ resources/views/guest/styles.blade.php
@@ -1,2 +1,10 @@
-<link rel="stylesheet" href="{{ asset('landing/style.css') }}" />
-<link href="{{ asset('landing/index.css') }}" rel="stylesheet" />
+<link rel="stylesheet" href="{{ asset('landing/styles/index.css') }}" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+/>
+<link
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+    rel="stylesheet"
+/>
