02f2c3d36b74e5aa9f5ddb8dd2931b569468fb63
Obed Nyakundi <real.obedpaul@gmail.com>
2025-01-18 13:37:16 +0300

    end of hero section

--- resources/views/guest/layout.blade.php
+++ resources/views/guest/layout.blade.php
@@ -1,37 +1,148 @@
 @include('guest.header')
-  <body>
-@include('guest.styles')
-    
-    <div>
-      <div class="frame2-container10">
-        <div class="frame2-frame2">
-          <div class="frame2-container11">
-            <div class="frame2-frame1">
-              <div class="frame2-container12">
-                <div class="frame2-container13">
+    <body>
+        <div class="main-frame">
 
-                    <!-- Body Area -->
-                  <div class="frame2-container14">
+            <!-- navigation -->
+            @include('guest.navigation')
+            <!-- end of navigation -->
 
-                    @yield('content')
-
-                  </div>
-                  <!-- End of Body Area -->
-
-                  <!-- Footer Area -->
-                  @include('guest.footer')
-                  <!-- End of Footer Area -->
-
-                  <!-- Navigation Area -->
-                  @include('guest.navigation')
-                  <!-- End of Navigation Area -->
-
+            <!-- hero section -->
+            <div class="frame2">
+                <div class="frame2-section">
+                    <h1>Ruto Must Go</h1>
+                    <p>If Kenya must change, Ruto must go first</p>
+                    <a href="#">
+                        <span>Latest Updates</span>
+                        <span class="material-symbols-outlined"
+                            >arrow_forward</span
+                        >
+                    </a>
                 </div>
-              </div>
             </div>
-          </div>
+
+            <div class="frame3">
+                <h1>Featured Stories</h1>
+                <div class="frame3-section">
+                    <div class="card">
+                        <div class="top">
+                            <img
+                                src="{{ asset('landing/Assets/tq_blzp55lz-d-z3i-200h.png') }}"
+                                alt=""
+                            />
+                        </div>
+                        <div class="bottom">
+                            <h5>Politics</h5>
+                            <h3>The State of Democracy</h3>
+                            <p>
+                                An in-depth analysis of Kenya'sdemocratic
+                                journey and currentchallenges
+                            </p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img
+                                src ="{{ asset('landing/Assets/tq_httdwkskzj-oll-400w.png') }}"
+                                alt=""
+                            />
+                        </div>
+                        <div class="bottom">
+                            <h5>Economy</h5>
+                            <h3>Economic Reforms</h3>
+                            <p>
+                                Understanding the impact of recenteconomic
+                                policies on daily life.
+                            </p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img
+                                src="{{ asset('landing/Assets/tq_vumowcotjv-c1ed-400w.png') }}"
+                                alt=""
+                            />
+                        </div>
+                        <div class="bottom">
+                            <h5>Gen z protest</h5>
+                            <h3>Youth Movement</h3>
+                            <p>
+                                How young Kenyans are driving social change
+                                across the nation.
+                            </p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="frame4">
+                <div class="frame4-top">
+                    <h1>popular topics</h1>
+                    <div class="frame4-top-section">
+                        <div class="card1"><a href="#">Abductions</a></div>
+                        <div class="card1"><a href="#">Economy</a></div>
+                        <div class="card1"><a href="#">Corruption</a></div>
+                        <div class="card1"><a href="#">Fufua ICC</a></div>
+                        <div class="card1"><a href="#">Gen Z Protests</a></div>
+                        <div class="card1">
+                            <a href="#">Reject Finance Bill 2024</a>
+                        </div>
+                        <div class="card1"><a href="#">Education</a></div>
+                        <div class="card1"><a href="#">Health</a></div>
+                    </div>
+                </div>
+                <div class="frame4-bottom">
+                    <div class="col1 col">
+                        <h2>Ruto Must Go</h2>
+                        <p>Your trusted source for news andanalysis</p>
+                        <p>
+                            &copy;<span id="getDate"></span>. All Rights
+                            Reserved.
+                        </p>
+                    </div>
+                    <div class="col2 col">
+                        <h2>Quick Links</h2>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Advertise</a></li>
+                        </ul>
+                    </div>
+                    <div class="col3 col">
+                        <h2>Categories</h2>
+                        <ul>
+                            <li><a href="#">Politics</a></li>
+                            <li><a href="#">Economy</a></li>
+                            <li><a href="#">Society</a></li>
+                        </ul>
+                    </div>
+                    <div class="col4 col">
+                        <h2>Follow Us</h2>
+                        <ul>
+                            <li>
+                                <a
+                                    href="https://www.facebook.com/jonathan.mwaniki.127"
+                                    ><span class="bi-facebook"></span
+                                ></a>
+                            </li>
+                            <li>
+                                <a href="https://x.com/Maestropuns"
+                                    ><span class="bi-twitter"></span
+                                ></a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/jonaahrojo/"
+                                    ><span class="bi-instagram"></span
+                                ></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </body>
+
+        <script src="{{ asset('landing/script.js') }}"></script>
+    </body>
 </html>
--- resources/views/guest/navigation.blade.php
+++ resources/views/guest/navigation.blade.php
@@ -1,61 +1,25 @@
-<div class="frame2-container43">
-                    <div class="frame2-container44">
-                      <div class="frame2-container45">
-                        <img
-                        src="{{ asset('landing/public/external/frame28-3v5n.svg') }}"
-                          alt="Frame28"
-                          class="frame2-frame7"
-                        />
-                        <div class="frame2-heading28">
-                          <span class="frame2-text51">Ruto Must Go</span>
-                        </div>
-
-                        <img style="width:40px; height:auto" src="{{ asset('images/kasongo_b.png') }}">
-
-                      </div>
-                      <div class="frame2-container46">
-
-                        <div class="frame2-container47">
-                            <a href="{{ route('home') }}">
-                                <span class="frame2-text52">Home</span>
-                            </a>
-                        </div>
-
-                        <div class="frame2-container48">
-                          <span class="frame2-text53">Politics</span>
-                        </div>
-
-                        <div class="frame2-container49">
-                          <span class="frame2-text54">Corruption</span>
-                        </div>
+<div class="frame1">
+                <div class="navbar-section1">
+                    <span class="material-symbols-outlined menu" id="menu"
+                        >menu</span>
                         
-                        <div class="frame2-container50">
-                          <span class="frame2-text55">Murders</span>
-                        </div>
+                    <h2>Ruto Must Go</h2>
+                </div>
+                <div class="navbar-section2">
+                    <ul>
+                        <li><a href="{{ route('home') }}">Home</a></li>
+                        <li><a href="#">Politics</a></li>
+                        <li><a href="#">Corruption</a></li>
+                        <li><a href="#">Murders</a></li>
+                        <li><a href="#">Abductions</a></li>
+                    </ul>
+                </div>
+                <div class="navbar-section3">
+                    <span class="material-symbols-outlined">search</span>
 
-                        <div class="frame2-container51">
-                            
-                                <span class="frame2-text56">Abductions</span>
-                            
-                        </div>
-
-                      </div>
-
-
-                      <div class="frame2-container52">
-                        <img
-                        src="{{ asset('landing/public/external/frame226-o5l.svg') }}"
-                          alt="Frame226"
-                          class="frame2-frame8"
-                        />
-
-                        <a href="{{ route('filament.admin.auth.login') }}">
-                            <img
-                            src="{{ asset('landing/public/external/frame229-u1k4.svg') }}"
-                              alt="Frame229"
-                              class="frame2-frame9"
-                            />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+                    <a href="{{ route('filament.admin.auth.login') }}">
+                      <span class="material-symbols-outlined"
+                        >account_circle</span>
+                    </a>
+                </div>
+</div>
